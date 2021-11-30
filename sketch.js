c2fb73c00c00aa24d84e04f96bf959d36580aa22
Nirmit272008 <87013183+Nirmit272008@users.noreply.github.com>
2021-11-30 12:01:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+var backgroundImg, ground;
+
+function preload() {
+  backgroundImg = loadImage("snow3.jpg");
+}
+
+function setup() {
+  var canvas = createCanvas(800,400);
+
+}
+
+function draw() {
+  background(backgroundImg);
+  
+ }
